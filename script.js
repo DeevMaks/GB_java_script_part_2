a2c0c6261649b6d5942d71ec207d6f696c7af887
maksim <antnvv@yandex.ru>
2021-11-22 19:55:33 +0800

    added styles for the page

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
+    { title: 'Shirt', image: 'images/shirt.jpeg', price: 150 },
+    { title: 'Socks', image: 'images/socks.jpeg', price: 50 },
+    { title: 'Jacket', image: 'images/jacket.jpeg', price: 350 },
+    { title: 'Shoes', image: 'images/shoes.jpeg', price: 250 },
 ];
 
-const $goodsList = document.querySelector('.goods-list');
+const $goodsList = document.querySelector('.goods__list');
 
-const renderGoodsItem = ({ title, price }) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+const renderGoodsItem = ({ title, image, price }) => {
+    return `<div class="goods__item">
+                <h3 class="item__header">${title}</h3>
+                <img class="item__image" src="${image}" alt="Goods Item: ${title}">
+                <p class="item__price">Price: ${price}</p>
+            </div>`;
 };
 
 const renderGoodsList = (list = goods) => {
@@ -16,7 +20,7 @@ const renderGoodsList = (list = goods) => {
         (item) => {
             return renderGoodsItem(item)
         }
-    ).join();
+    ).join('');
 
     $goodsList.insertAdjacentHTML('beforeend', goodsList);
 }
